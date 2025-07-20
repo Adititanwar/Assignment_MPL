ebd2db14aae885992529ee4582e2d93330c174d3
Adititanwar <adititanwar434@gmail.com>
2025-07-20 11:19:34 +0530

    added the endpoints of update and delete blogs

--- Assignment /controllers/blogs.controller.js
+++ Assignment /controllers/blogs.controller.js
@@ -13,3 +13,40 @@ exports.createBlog = async (req, res) => {
     res.status(500).json({ message: 'Blog creation failed', error: err.message });
   }
 };
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) return res.status(404).json({ message: 'Blog not found' });
+
+    if (blog.author.toString() !== req.user._id.toString())
+      return res.status(403).json({ message: 'Not allowed to update this blog' });
+
+    blog.title = req.body.title || blog.title;
+    blog.body = req.body.body || blog.body;
+    await blog.save();
+
+    res.json({ blog });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to update blog' });
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) return res.status(404).json({ message: 'Blog not found' });
+
+    const isOwner = blog.author.toString() === req.user._id.toString();
+    const isAdmin = req.user.role === 'admin';
+
+    if (!isOwner && !isAdmin) {
+      return res.status(403).json({ message: 'Only the owner or admin can delete this blog' });
+    }
+
+    await blog.deleteOne();
+    res.json({ message: 'Blog deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete blog' });
+  }
+};
--- Assignment /routes/blogs.route.js
+++ Assignment /routes/blogs.route.js
@@ -4,5 +4,7 @@ const blogCtrl = require('../controllers/blogs.controller');
 const { protect } = require('../middleware/auth.middleware');
 
 router.post('/', protect, blogCtrl.createBlog);
+router.put('/:id', protect, blogCtrl.updateBlog);
+router.delete('/:id', protect, blogCtrl.deleteBlog);
 
 module.exports = router;
