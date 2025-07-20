61f36db7cd886c5ae2a3e431d48389969832f4e5
Adititanwar <adititanwar434@gmail.com>
2025-07-20 09:13:56 +0530

    Initial commit with index.js and project structure

---
+++ Assignment /index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Our app is running');
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error');
+    });
